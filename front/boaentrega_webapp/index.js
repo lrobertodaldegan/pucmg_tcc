21747f6d251b9332c5731b667a92fff5bcddd716
Lucas Roberto <lrobertodaldegan@hotmail.com>
2022-03-01 18:16:58 -0300

    adicionando pg de formulário pra fornecedores

--- front/boaentrega_webapp/index.js
+++ front/boaentrega_webapp/index.js
@@ -1,15 +1,22 @@
 //EXTERNAL MODULES
 const express = require("express");
+const session = require("express-session");
 const path = require("path");
+const axiosRequest = require('axios');
+const bodyParser = require('body-parser');
 
 //VARIABLES
 const app = express();
 const port = process.env.PORT || "7070";
 
+let data = null;
+
 //CONFIGURATION
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(session({secret: 'boaEntregaTcc', resave: false, saveUninitialized: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 
 //ROUTES
 app.get("/", (req, res) => res.render("pages/home", {title: "Home"}));
@@ -17,7 +24,8 @@ app.get("/clients", (req, res) => res.render("pages/clients", {title: "Clientes"
 app.get("/stores", (req, res) => res.render("pages/stores", {title: "Estoque"}));
 app.get("/settings", (req, res) => res.render("pages/settings", {title: "Configurações"}));
 app.get("/suppliers", (req, res) => {
-    var data = {
+    //TODO integrar com API
+    data = {
         title: "Fornecedores",
         itens:[
             {
@@ -83,9 +91,64 @@ app.get("/suppliers", (req, res) => {
 
     res.render("pages/suppliers", data)
 });
+app.get("/suppliers/form", (req, res) => {
+    let obj = null;
+
+    if(req.query.id != null){
+        data.suppliers.forEach((item) => {
+            if(item.id == req.query.id){
+                obj = item;
+                return;
+            }
+        });
+    }
+
+    res.render("pages/form/suppliers", obj);
+});
+app.post("/suppliers/save", (req, res) => {
+    let obj = req.body;
+    let itsNotOk = false;
+
+    if(obj != null){
+        if(obj.exists){
+            axiosRequest.patch("url", obj)
+                        .catch((err) => {
+                            console.error(err);
+                            itsNotOk = true;
+                        });
+        } else {
+            axiosRequest.post("url", obj)
+                        .catch((err) => {
+                            console.error(err);
+                            itsNotOk = true;
+                        });
+        }
+    }
+
+    if(itsNotOk) {
+        req.session.error = "Houve um erro ao tentar salvar a operação. Tente novamente mais tarde!";
+        res.redirect("/error");
+    } else { 
+        res.redirect("/suppliers");
+    }
+});
+
+//Generic error route
+app.all('/error', (req, res) => {
+    let msg = req.session.error;
+
+    res.status(500).render('pages/error', {
+        title: "Ops! Algo deu errado!", 
+        message: msg != null && msg != undefined ? msg : "Tente novamente mais tarde..."
+    });
+});
+
 //Routes Error handlin
 app.all('*', (req, res) => {
-    res.status(404).render('pages/error', {title: "Ops! Algo deu errado!"});
+    res.status(404).render('pages/error', {
+        title: "Ops! Algo deu errado!", 
+        message: "O conteúdo que você procura não pôde ser encontrado! Tente novamente mais tarde..."
+    });
 });
 
 //SERVER ACTIVATION
